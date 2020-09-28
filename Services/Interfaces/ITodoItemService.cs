836e17c83b843f6c53e7f170fc2442fad60cd54c
施華桐 <gundambox.shi@gmail.com>
2020-09-29 00:49:41 +0800

    實作邏輯層

---
+++ Services/Interfaces/ITodoItemService.cs
@@ -0,0 +1,33 @@
+﻿using Entities;
+using Entities.ViewModels;
+using Repository.CustomSearch.Interfaces;
+using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Services.Interfaces
+{
+    public interface ITodoItemService
+    {
+        #region Create
+        TodoItemGetViewModel Create(TodoItem obj);
+        Task<TodoItemGetViewModel> CreateAsync(TodoItem obj);
+        #endregion
+        #region Read
+        TodoItemGetViewModel Get(Expression<Func<TodoItem, bool>> predicate);
+        Task<TodoItemGetViewModel> GetAsync(Expression<Func<TodoItem, bool>> predicate);
+        ListOf<TodoItemGetViewModel> List(IBaseCustomSearch search);
+        Task<ListOf<TodoItemGetViewModel>> ListAsync(IBaseCustomSearch search);
+        #endregion
+        #region Update
+        TodoItemGetViewModel Update(TodoItem obj);
+        Task<TodoItemGetViewModel> UpdateAsync(TodoItem obj);
+        #endregion
+        #region Delete
+        void Delete(TodoItem obj);
+        void Delete(Expression<Func<TodoItem, bool>> predicate);
+        Task DeleteAsync(Expression<Func<TodoItem, bool>> predicate);
+        Task DeleteAsync(TodoItem obj);
+        #endregion
+    }
+}
---
+++ Services/TodoItemService.cs
@@ -0,0 +1,110 @@
+﻿using AutoMapper;
+using Entities;
+using Entities.ViewModels;
+using Repository.CustomSearch.Interfaces;
+using Repository.Interfaces;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class TodoItemService : ITodoItemService
+    {
+        private IMapper _mapper;
+        private readonly IRepository<TodoItem> _repository;
+
+        public TodoItemService(IMapper mapper, IRepository<TodoItem> repository)
+        {
+            _mapper = mapper;
+            _repository = repository;
+        }
+
+        public TodoItemGetViewModel Create(TodoItem obj)
+        {
+            var item = _repository.Create(obj);
+            var viewmodel = _mapper.Map<TodoItemGetViewModel>(item);
+            return viewmodel;
+        }
+
+        public async Task<TodoItemGetViewModel> CreateAsync(TodoItem obj)
+        {
+            var item = await _repository.CreateAsync(obj);
+            var viewmodel = _mapper.Map<TodoItemGetViewModel>(item);
+            return viewmodel;
+        }
+
+        public void Delete(TodoItem obj)
+        {
+            _repository.Delete(obj);
+        }
+
+        public void Delete(Expression<Func<TodoItem, bool>> predicate)
+        {
+            _repository.Delete(predicate);
+        }
+
+        public async Task DeleteAsync(Expression<Func<TodoItem, bool>> predicate)
+        {
+            await _repository.DeleteAsync(predicate);
+        }
+
+        public async Task DeleteAsync(TodoItem obj)
+        {
+            await _repository.DeleteAsync(obj);
+        }
+
+        public TodoItemGetViewModel Get(Expression<Func<TodoItem, bool>> Get)
+        {
+            var item = _repository.Get(Get);
+            var viewmodel = _mapper.Map<TodoItemGetViewModel>(item);
+            return viewmodel;
+        }
+
+        public async Task<TodoItemGetViewModel> GetAsync(Expression<Func<TodoItem, bool>> predicate)
+        {
+            var item = await _repository.GetAsync(predicate);
+            var viewmodel = _mapper.Map<TodoItemGetViewModel>(item);
+            return viewmodel;
+        }
+
+        public ListOf<TodoItemGetViewModel> List(IBaseCustomSearch search)
+        {
+            var items = _repository.List(search);
+            var viewmodel = new ListOf<TodoItemGetViewModel>
+            {
+                Collection = _mapper.Map<IEnumerable<TodoItemGetViewModel>>(items),
+                Total = items.Count()
+            };
+            return viewmodel;
+        }
+
+        public async Task<ListOf<TodoItemGetViewModel>> ListAsync(IBaseCustomSearch search)
+        {
+            var items = await _repository.ListAsync(search);
+            var viewmodel = new ListOf<TodoItemGetViewModel>
+            {
+                Collection = _mapper.Map<IEnumerable<TodoItemGetViewModel>>(items),
+                Total = items.Count()
+            };
+            return viewmodel;
+        }
+
+        public TodoItemGetViewModel Update(TodoItem obj)
+        {
+            var item = _repository.Update(obj);
+            var viewmodel = _mapper.Map<TodoItemGetViewModel>(item);
+            return viewmodel;
+        }
+
+        public async Task<TodoItemGetViewModel> UpdateAsync(TodoItem obj)
+        {
+            var item = await _repository.UpdateAsync(obj);
+            var viewmodel = _mapper.Map<TodoItemGetViewModel>(item);
+            return viewmodel;
+        }
+    }
+}
